c09e55f63ce011b2b77fca1593ed2d046e29f90c
Michael Liu <knispo6781@gmail.com>
2018-09-19 11:47:48 -0400

    Login interface in progress

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,20 +1,24 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 
 import { AppComponent } from './app.component';
 import { RideswipeComponent } from './components/rideswipe/rideswipe.component';
 import { LandingComponent } from './components/landing/landing.component';
+import { LoginComponent } from './components/login/login.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
     RideswipeComponent,
-    LandingComponent
+    LandingComponent,
+    LoginComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
