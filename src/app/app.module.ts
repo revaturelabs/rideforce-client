600a81ca269728b9913b700278e9c07fe6609113
Arthur Panlilio <PanlilioArthur@yahoo.com>
2018-09-19 10:56:19 -0400

    added swiping cards

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,20 +1,24 @@
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { NgModule } from '@angular/core';
 
 
 import { AppComponent } from './app.component';
 import { RideswipeComponent } from './components/rideswipe/rideswipe.component';
 import { LandingComponent } from './components/landing/landing.component';
+import { UsercardComponent } from './components/usercard/usercard.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
     RideswipeComponent,
-    LandingComponent
+    LandingComponent,
+    UsercardComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
