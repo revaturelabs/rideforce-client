9b54d2b6fae9331c5e3acaecf57cc7c24b8d349b
tiffannie <tiffannie.macdonald@gmail.com>
2018-09-20 10:14:46 -0400

    finished adding models (types/enums)

--- src/app/models/contact-info.model.ts
+++ src/app/models/contact-info.model.ts
@@ -1,13 +1,12 @@
 export interface ContactInfo {
-    {
     /**
      * The ID of this contact info entry in the database.
      */
-    id: number;
+    id: Number;
     type: ContactInfoType;
     /**
      * The details specifying the user's username/phone number/etc. on the
      * service.
      */
-    info: string;
+    info: String;
 }
