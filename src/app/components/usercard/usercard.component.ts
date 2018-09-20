454c2b811f83c802f35e4d8e925cabd50b420f87
Arthur Panlilio <PanlilioArthur@yahoo.com>
2018-09-20 09:48:54 -0400

    tried to add sliding bio panel thing

--- src/app/components/usercard/usercard.component.ts
+++ src/app/components/usercard/usercard.component.ts
@@ -1,72 +1,122 @@
 
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild, ElementRef, Input } from '@angular/core';
 import { UserModel } from '../../models/user.model';
 import { AddressModel } from '../../models/address.model';
 import { SwipecardModel } from '../../models/swipecard.model';
+import { animate, state, style, transition, trigger } from '@angular/animations';
 
-import { trigger, keyframes, animate, transition } from '@angular/animations';
 
-
+type PaneType = 'main' | 'bio';
 
 @Component({
-  selector: 'app-usercard',
-  templateUrl: './usercard.component.html',
-  styleUrls: ['./usercard.component.css']
+    selector: 'app-usercard',
+    templateUrl: './usercard.component.html',
+    styleUrls: ['./usercard.component.css'],
+    animations: [
+        trigger('slide', [
+            state('up', style({ transform: 'translateY(0)' })),
+            state('down', style({ transform: 'translateY(-2%)' })),
+            transition('* => *', animate(100)),
+        ])]
 })
 export class UsercardComponent implements OnInit {
 
+    @Input() activePane: PaneType = 'main';
 
- // constant for swipe action: left or right
- SWIPE_ACTION = { LEFT: 'swipeleft', RIGHT: 'swiperight' };
-   // our list of avatars
- swipecards: SwipecardModel[] = [
-  {
-        user: {
-            userID: 1,
-            firstname: 'kristy',
-            lastname: 'Kreme',
-            email: 'email@mail.com',
-            roleID: 1,
-            officeID: 2,
-            photourl: 'http://semantic-ui.com/images/avatar2/large/kristy.png',
+    // constant for swipe action: left or right
+    SWIPE_ACTION = { LEFT: 'swipeleft', RIGHT: 'swiperight' };
+    // our list of avatars
+    swipecards: SwipecardModel[] = [
+        {
+            user: {
+                userID: 1,
+                firstname: 'kristy',
+                lastname: 'Kreme',
+                email: 'email@mail.com',
+                roleID: 1,
+                officeID: 2,
+                photourl: 'http://semantic-ui.com/images/avatar2/large/kristy.png',
+            },
+            visible: false
         },
-        visible: true
+        {
+            user: {
+                userID: 1,
+                firstname: 'Frank',
+                lastname: 'Frankerson',
+                email: 'f@mail.com',
+                roleID: 1,
+                officeID: 2,
+                photourl: 'http://semantic-ui.com/images/avatar2/large/matthew.png',
+            },
+            visible: false
+        }
+    ];
+
+    state = 'up';
+
+    public mobile = false;
+    public bioCollapsed = true;
+
+    @ViewChild('swipeMain') swipeCardMain: ElementRef;
+    @ViewChild('swipeBio') swipeCardBio: ElementRef;
+
+    constructor() { }
+
+    ngOnInit() {
+        if (window.screen.width <= 430) { // 768px portrait
+            this.mobile = true;
+        }
+        if (this.swipecards.length > 0) {
+            this.swipecards[0].visible = true;
+        }
     }
-];
+    test(event: any) {
+        if (this.state === 'down') {
+            this.swipeCardMain.nativeElement.classList.add('hidden');
+        } else {
+            this.swipeCardMain.nativeElement.classList.remove('hidden');
+        }
+    }
+    test2(event: any) {
 
+        if (this.state === 'up') {
+            this.swipeCardBio.nativeElement.classList.add('hidden');
+        } else {
+            console.log(event);
+            this.swipeCardBio.nativeElement.classList.remove('hidden');
+        }
+    }
 
+    animateMe() {
+        this.state = (this.state === 'up' ? 'down' : 'up');
+        console.log(this.state);
+    }
 
-  animationState: string;
+    // action triggered when user swipes
+    swipe(currentIndex: number, action = this.SWIPE_ACTION.RIGHT, event) {
+        // out of range
+        if (currentIndex > this.swipecards.length || currentIndex < 0) {
+            return;
+        }
 
-  constructor() { }
+        let nextIndex = 0;
 
-  ngOnInit() {
-  }
+        // swipe right, next avatar
+        if (action === this.SWIPE_ACTION.RIGHT) {
+            const isLast = currentIndex === this.swipecards.length - 1;
+            nextIndex = isLast ? 0 : currentIndex + 1;
+        }
 
-   // action triggered when user swipes
-   swipe(currentIndex: number, action = this.SWIPE_ACTION.RIGHT, event) {
-       // out of range
-       if (currentIndex > this.swipecards.length || currentIndex < 0) {
-          return;
-       }
+        // swipe left, previous avatar
+        if (action === this.SWIPE_ACTION.LEFT) {
+            const isFirst = currentIndex === 0;
+            nextIndex = isFirst ? this.swipecards.length - 1 : currentIndex - 1;
+        }
 
-       let nextIndex = 0;
-
-       // swipe right, next avatar
-       if (action === this.SWIPE_ACTION.RIGHT) {
-           const isLast = currentIndex === this.swipecards.length - 1;
-           nextIndex = isLast ? 0 : currentIndex + 1;
-       }
-
-       // swipe left, previous avatar
-       if (action === this.SWIPE_ACTION.LEFT) {
-           const isFirst = currentIndex === 0;
-           nextIndex = isFirst ? this.swipecards.length - 1 : currentIndex - 1;
-       }
-
-       // toggle avatar visibility
-       this.swipecards.forEach((x, i) => x.visible = (i === nextIndex));
-   }
+        // toggle avatar visibility
+        this.swipecards.forEach((x, i) => x.visible = (i === nextIndex));
+    }
 
 
 }
--- src/app/models/swipecard.model.ts
+++ src/app/models/swipecard.model.ts
@@ -2,5 +2,5 @@ import { UserModel } from './user.model';
 
 export class SwipecardModel {
     user: UserModel;
-    visible: boolean;
+    visible = false;
 }
