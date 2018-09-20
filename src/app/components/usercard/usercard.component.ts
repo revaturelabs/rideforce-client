847304adc9545c19b4867c4b273fd557bb96e7cb
Arthur Panlilio <PanlilioArthur@yahoo.com>
2018-09-20 12:17:12 -0400

    fixed the mobile view for swipecards

--- src/app/components/usercard/usercard.component.ts
+++ src/app/components/usercard/usercard.component.ts
@@ -3,29 +3,18 @@ import { Component, OnInit, ViewChild, ElementRef, Input } from '@angular/core';
 import { User } from '../../models/user.model';
 import { AddressModel } from '../../models/address.model';
 import { SwipecardModel } from '../../models/swipecard.model';
-import { animate, state, style, transition, trigger } from '@angular/animations';
 
-
-type PaneType = 'main' | 'bio';
 
 @Component({
     selector: 'app-usercard',
     templateUrl: './usercard.component.html',
-    styleUrls: ['./usercard.component.css'],
-    animations: [
-        trigger('slide', [
-            state('up', style({ transform: 'translateY(0)' })),
-            state('down', style({ transform: 'translateY(-2%)' })),
-            transition('* => *', animate(100)),
-        ])]
+    styleUrls: ['./usercard.component.css']
 })
 export class UsercardComponent implements OnInit {
 
-    @Input() activePane: PaneType = 'main';
-
     // constant for swipe action: left or right
     SWIPE_ACTION = { LEFT: 'swipeleft', RIGHT: 'swiperight' };
-    // our list of avatars
+    // our list of swipecards: DUMMY DATA
     swipecards: SwipecardModel[] = [
         {
             user: {
@@ -58,13 +47,29 @@ export class UsercardComponent implements OnInit {
                 photoUrl: 'http://semantic-ui.com/images/avatar2/large/matthew.png'
             },
             visible: false
+        }, {
+            user: {
+                id: 1,
+                firstName: 'Jimbo',
+                lastName: 'Jank',
+                email: 'email@mail.com',
+                address: '123',
+                office: '1',
+                batchEnd: '1',
+                cars: [],
+                contactInfo: [],
+                active: true,
+                photoUrl: 'http://semantic-ui.com/images/avatar/large/chris.jpg'
+            },
+            visible: false
         }
     ];
 
-    state = 'up';
 
     public mobile = false;
-    public bioCollapsed = true;
+
+    currentSwipeCard: SwipecardModel;
+    currentIndex = 0;
 
     @ViewChild('swipeMain') swipeCardMain: ElementRef;
     @ViewChild('swipeBio') swipeCardBio: ElementRef;
@@ -75,49 +80,48 @@ export class UsercardComponent implements OnInit {
         if (window.screen.width <= 430) { // 768px portrait
             this.mobile = true;
         }
+        // Sets the current swipe card to the first element of the array if the array has something in it.
         if (this.swipecards.length > 0) {
-            this.swipecards[0].visible = true;
+            this.currentSwipeCard = this.swipecards[0];
         }
     }
-    hideMain(event: any) {
-        if (this.state === 'down') {
-            this.swipeCardBio.nativeElement.classList.remove('hidden');
+
+    /**
+     * Hides the card picture
+     *
+     * @param hide
+     */
+    hideImage(hide: boolean) {
+        if (hide) {
             this.swipeCardMain.nativeElement.classList.add('hidden');
         } else {
             this.swipeCardMain.nativeElement.classList.remove('hidden');
-            this.swipeCardBio.nativeElement.classList.add('hidden');
         }
-    }
-
-
-    animateMe() {
-        this.state = (this.state === 'up' ? 'down' : 'up');
-        console.log(this.state);
     }
 
     // action triggered when user swipes
-    swipe(currentIndex: number, action = this.SWIPE_ACTION.RIGHT, event) {
-        // out of range
-        if (currentIndex > this.swipecards.length || currentIndex < 0) {
-            return;
-        }
-
-        let nextIndex = 0;
+    swipe(action = this.SWIPE_ACTION.RIGHT, event) {
 
         // swipe right, next avatar
         if (action === this.SWIPE_ACTION.RIGHT) {
-            const isLast = currentIndex === this.swipecards.length - 1;
-            nextIndex = isLast ? 0 : currentIndex + 1;
+            if (this.currentIndex - 1 < 0) {
+                this.currentIndex = this.swipecards.length - 1;
+            } else {
+                this.currentIndex--;
+            }
         }
 
         // swipe left, previous avatar
         if (action === this.SWIPE_ACTION.LEFT) {
-            const isFirst = currentIndex === 0;
-            nextIndex = isFirst ? this.swipecards.length - 1 : currentIndex - 1;
+            if (this.currentIndex + 1 > this.swipecards.length - 1) {
+                this.currentIndex = 0;
+            } else {
+                this.currentIndex++;
+            }
         }
 
-        // toggle avatar visibility
-        this.swipecards.forEach((x, i) => x.visible = (i === nextIndex));
+        // Load currentSwipeCard with the swipecard currentindex
+        this.currentSwipeCard = this.swipecards[this.currentIndex];
     }
 
 
