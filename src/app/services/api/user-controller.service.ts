dfdec4ecf9157dcd469b558bd398bd4457751bce
Michael Liu <knispo6781@gmail.com>
2018-09-21 09:40:29 -0400

    sped up animation of login

--- src/app/services/api/user-controller.service.ts
+++ src/app/services/api/user-controller.service.ts
@@ -1,10 +1,15 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
 
 @Injectable()
 export class UserControllerService {
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
 
+  login(email: string, password: string): Observable<any>{
+    return this.http.post<any>("http://localhost:8081/Reimb/login", {email: email, password: password});
+  }
   
 
 }
