630ac882705cd8df223bfec05f250f1e20f1239e
tiffannie <tiffannie.macdonald@gmail.com>
2018-09-21 16:36:18 -0400

    beginning of map

--- src/app/components/map/map.component.ts
+++ src/app/components/map/map.component.ts
@@ -1,10 +1,9 @@
-import { Component, OnInit, ViewChild } from '@angular/core';
+import { Component, OnInit, ViewChild, NgZone } from '@angular/core';
 import { NgbTabset } from '@ng-bootstrap/ng-bootstrap';
 import { MapsControllerService } from '../../services/api/maps-controller.service';
 import { Location } from './../../models/location.model';
 import { User } from '../../models/user.model';
-import { Marker } from '../../models/marker.model';
-
+import { } from '@types/googlemaps';
 @Component({
   selector: 'app-map',
   templateUrl: './map.component.html',
@@ -21,18 +20,50 @@ export class MapComponent implements OnInit {
   private dist: number;
   private time: number;
 
-  // ------------------------------------------------------------
-  public origin: google.maps.LatLngLiteral;
-  public destination: google.maps.LatLngLiteral;
+  latitude: any;
+  longitude: any;
 
-  driversNearBy: Marker[] = [];
-  currentUser: User;
-  controlmap;
+  @ViewChild('gmap') gmapElement: any;
+  map: google.maps.Map;
+
+  isTracking = false;
+
+  currentLat: any;
+  currentLong: any;
+
+  iconBase = 'https://maps.google.com/mapfiles/kml/shapes/';
+  marker: google.maps.Marker; 
+
+  markerTypes = [
+    {
+      text: "Parking", value: "parking_lot_maps.png"
+    }
+    // ,
+    // {
+    //   text: "Library", value: "library_maps.png"
+    // },
+    // {
+    //   text: "Information", value: "info-i_maps.png"
+    // }
+  ];
+
+  selectedMarkerType: string = "parking_lot_maps.png";
+
+  isHidden = false;
 
   constructor(private mapService: MapsControllerService) { }
 
   ngOnInit() {
+  }
 
+  ngAfterContentInit() {
+    let mapProp = {
+      center: new google.maps.LatLng(38.9586, -77.3570),
+      zoom: 15,
+      mapTypeId: google.maps.MapTypeId.ROADMAP
+    };
+    this.map = new google.maps.Map(this.gmapElement.nativeElement, mapProp);
+    this.findMe();
   }
 
   public getRoute() {
@@ -44,29 +75,117 @@ export class MapComponent implements OnInit {
         console.log(this.time);
       }
     )
-
   }
-  // -  - - - - - - ------------------------------------------------
-  mapReady($event: any) {
-    this.controlmap = $event;
-    this.controlmap.fitBounds({
-      east: -66.94,
-      north: 49.38,
-      west: -124.39,
-      south: 25.82
+
+  setMapType(mapTypeId: string) {
+    this.map.setMapTypeId(mapTypeId)
+  }
+
+  setCenter(e:any){
+    this.map.setCenter(new google.maps.LatLng(this.latitude, this.longitude));
+
+    let location = new google.maps.LatLng(this.latitude, this.longitude);
+
+    let marker = new google.maps.Marker({
+      position: location,
+      map: this.map,
+      title: 'Got you!'
+    });
+
+    marker.addListener('click', this.simpleMarkerHandler);
+
+    marker.addListener('click', () => {
+      this.markerHandler(marker);
     });
   }
 
-  updateZoomLevel() {
-    if (this.controlmap.zoom <= 12) {
-      this.driversNearBy = [];
-      this.currentUser = null;
+  simpleMarkerHandler() {
+    alert('Simple Component\'s function...');
+  }
+
+  markerHandler(marker: google.maps.Marker) {
+    alert('Marker\'s Title: ' + marker.getTitle());
+  }
+
+  showCustomMarker() {
+    this.map.setCenter(new google.maps.LatLng(this.latitude, this.longitude));
+
+    let location = new google.maps.LatLng(this.latitude, this.longitude);
+
+    console.log(`selected marker: ${this.selectedMarkerType}`);
+
+    let marker = new google.maps.Marker({
+      position: location,
+      map: this.map,
+      icon: this.iconBase + this.selectedMarkerType,
+      title: 'Got you!'
+    });
+  }
+  
+  toggleMap() {
+    this.isHidden = !this.isHidden;
+
+    this.gmapElement.nativeElement.hidden = this.isHidden;
+  }
+
+  findMe() {
+    if (navigator.geolocation) {
+      navigator.geolocation.getCurrentPosition((position) => {
+        this.showPosition(position);
+      });
+    } else {
+      alert("Geolocation is not supported by this browser.");
     }
   }
 
+  trackMe() {
+    if (navigator.geolocation) {
+      this.isTracking = true;
+      navigator.geolocation.watchPosition((position) => {
+        this.showTrackingPosition(position);
+      });
+    } else {
+      alert("Geolocation is not supported by this browser.");
+    }
+  }
 
-  // ---------------------------------------------------------------
+  showPosition(position) {
+    this.currentLat = position.coords.latitude;
+    this.currentLong = position.coords.longitude;
 
+    let location = new google.maps.LatLng(position.coords.latitude, position.coords.longitude);
+    this.map.panTo(location);
 
+    if (!this.marker) {
+      this.marker = new google.maps.Marker({
+        position: location,
+        map: this.map,
+        title: 'Got you!'
+      });
+    }
+    else {
+      this.marker.setPosition(location);
+    }
+  }
+
+  showTrackingPosition(position) {
+    console.log(`tracking postion:  ${position.coords.latitude} - ${position.coords.longitude}`);
+    this.currentLat = position.coords.latitude;
+    this.currentLong = position.coords.longitude;
+
+    let location = new google.maps.LatLng(position.coords.latitude, position.coords.longitude);
+    this.map.panTo(location);
+
+    if (!this.marker) {
+      this.marker = new google.maps.Marker({
+        position: location,
+        map: this.map,
+        title: 'Got you!'
+      });
+    }
+    else {
+      this.marker.setPosition(location);
+    }
+  }
 
 }
