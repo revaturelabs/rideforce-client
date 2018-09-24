93252c6c45ea1deaf46ced6a52f6584d6c63d149
tiffannie <tiffannie.macdonald@gmail.com>
2018-09-24 10:05:51 -0400

    added login function in login component

--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { AuthService } from '../../services/auth.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-login',
@@ -7,14 +9,22 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginComponent implements OnInit {
 
-  constructor() { }
+  userEmail: string;
+  userPass: string;
+
+  constructor(private authService: AuthService, private route: Router) { }
 
   ngOnInit() {
   }
 
+  login(){
+    this.authService.authenticate(this.userEmail, this.userPass).subscribe(
+      () => {
+        this.route.navigate(['/home']);
+      }
+      // TODO if an error is returned, return the error message to user
 
-  // passLogin: string;
-
-  
+    );
+  }  
 
 }
