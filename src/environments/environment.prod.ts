1b2b73a6f689a06dc0336552d8b31ffefe1da89f
tiffannie <tiffannie.macdonald@gmail.com>
2018-09-26 10:23:47 -0400

    fixing rds url

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: true,
-  apiUrl: 'http://ec2-18-209-24-207.compute-1.amazonaws.com:2222'
+  apiUrl: 'http://ec2-35-174-153-234.compute-1.amazonaws.com:2222'
 };
