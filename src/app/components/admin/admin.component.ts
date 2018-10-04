75f2360d80be03e156c33c91cfb3dab139733274
11crandall <11ccrandall@gmail.com>
2018-10-04 12:58:47 -0500

    Implemented most functionality, aside form actual encription

--- src/app/components/admin/admin.component.ts
+++ src/app/components/admin/admin.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { UserControllerService } from '../../services/api/user-controller.service';
+import { Office } from '../../models/office.model';
 
 @Component({
   selector: 'app-admin',
@@ -9,22 +10,38 @@ import { UserControllerService } from '../../services/api/user-controller.servic
 export class AdminComponent implements OnInit {
 
   registrationKey: string;
+  officeObjectArray: Office[] = [];
+  batchEnd: string;
+  officeObject;
+
+  officeObjectEncrypted: string;
+  batchEndEncrypted: string;
 
   constructor(private userService: UserControllerService) { }
 
   ngOnInit() {
+    this.getOffices();
   }
 
   getRegistrationKey(){
     this.userService.getRegistrationKey().subscribe(
       data => {
         this.registrationKey = data;
+        this.dateLocationEncryption();
       }
     )
   }
 
   dateLocationEncryption(){
-    
+
+    this.officeObjectEncrypted = this.officeObject;
+    this.batchEndEncrypted = this.batchEnd;
   }
 
+  getOffices() {
+    this.userService.getAllOffices().subscribe(data => {
+      this.officeObjectArray = data;
+      this.officeObject = '';
+    });
+  }
 }
