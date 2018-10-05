b20f7bbab705fce187ecd701b8da8f00f9331ec2
11crandall <11ccrandall@gmail.com>
2018-10-05 12:42:10 -0500

    Finished trainer-side encoding

--- src/app/components/admin/admin.component.ts
+++ src/app/components/admin/admin.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { UserControllerService } from '../../services/api/user-controller.service';
 import { Office } from '../../models/office.model';
+import { toBase64String } from '@angular/compiler/src/output/source_map';
 
 @Component({
   selector: 'app-admin',
@@ -14,8 +15,7 @@ export class AdminComponent implements OnInit {
   batchEnd: string;
   officeObject;
 
-  officeObjectEncrypted: string;
-  batchEndEncrypted: string;
+  encryptedLocationDate: string;
 
   constructor(private userService: UserControllerService) { }
 
@@ -33,9 +33,7 @@ export class AdminComponent implements OnInit {
   }
 
   dateLocationEncryption(){
-
-    this.officeObjectEncrypted = this.officeObject;
-    this.batchEndEncrypted = this.batchEnd;
+    this.encryptedLocationDate = btoa(this.officeObject + "~" + this.batchEnd);
   }
 
   getOffices() {
@@ -45,3 +43,30 @@ export class AdminComponent implements OnInit {
     });
   }
 }
+/*
+QXJsaW5ndG9ufjIwMTgtMDEtMDE=
+MjAxOC0wMS0wMX5Bcmxpbmd0b24=
+UmVzdG9ufjIwMTgtMDEtMDE=
+MjAxOC0wMS0wMX5SZXN0b24=
+/UmVzdG9ufjE5OTMtMDItMTA=
+/MTk5My0wMi0xMH5SZXN0b24=
+VGFtcGF+MjAxOC0wMS0wMQ==
+MjAxOC0wMS0wMX5UYW1wYQ==
+/VGFtcGF+MTk5My0wMi0xMA==
+/MTk5My0wMi0xMH5UYW1wYQ==
+Rmx1c2hpbmd+MjAxOC0wMS0wMQ==
+MjAxOC0wMS0wMX5GbHVzaGluZw==
+TmV3IFlvcmt+MjAxOC0wMS0wMQ==
+MjAxOC0wMS0wMX5OZXcgWW9yaw==
+
+QXJsaW5ndG9ufjIwMTItMTItMTI=
+MjAxMi0xMi0xMn5Bcmxpbmd0b24=
+UmVzdG9ufjIwMTItMTItMTI=
+MjAxMi0xMi0xMn5SZXN0b24=
+VGFtcGF+MjAxMi0xMi0xMg==
+MjAxMi0xMi0xMn5UYW1wYQ==
+Rmx1c2hpbmd+MjAxMi0xMi0xMg==
+MjAxMi0xMi0xMn5GbHVzaGluZw==
+TmV3IFlvcmt+MjAxMi0xMi0xMg==
+MjAxMi0xMi0xMn5OZXcgWW9yaw==
+*/
