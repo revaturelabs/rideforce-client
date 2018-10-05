1f754326667be3b6e626ea48144ac432ef6ceda7
John Jacko <jljacko@outlook.com>
2018-10-05 14:27:55 -0400

    Reduced Failure in testing down to 5 failures

--- src/app/components/view-profile/view-profile.component.spec.ts
+++ src/app/components/view-profile/view-profile.component.spec.ts
@@ -1,22 +1,30 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { HttpHandler, HttpClient } from '@angular/common/http';
+import { UserControllerService } from '../../services/api/user-controller.service';
+import { DateFormatPipe } from '../../pipes/date-format.pipe';
 
 import { ViewProfileComponent } from './view-profile.component';
 
 describe('ViewProfileComponent', () => {
   let component: ViewProfileComponent;
-  let fixture: ComponentFixture<ViewProfileComponent>;
+  // let fixture: ComponentFixture<ViewProfileComponent>;
 
-  beforeEach(async(() => {
-    TestBed.configureTestingModule({
-      declarations: [ ViewProfileComponent ]
-    })
-    .compileComponents();
-  }));
+  // beforeEach(async(() => {
+  //   TestBed.configureTestingModule({
+  //     declarations: [ ViewProfileComponent ]
+  //   })
+  //   .compileComponents();
+  // }));
 
+  // beforeEach(() => {
+  //   fixture = TestBed.createComponent(ViewProfileComponent);
+  //   component = fixture.componentInstance;
+  //   fixture.detectChanges();
+  // });
   beforeEach(() => {
-    fixture = TestBed.createComponent(ViewProfileComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
+    TestBed.configureTestingModule({providers: [HttpHandler, HttpClient, DateFormatPipe,
+       UserControllerService, ViewProfileComponent]});
+    component = TestBed.get(ViewProfileComponent);
   });
 
   it('should create', () => {
