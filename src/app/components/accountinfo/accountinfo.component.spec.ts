c7f366056fb163b9b036ce57589edf89ef5a99df
Cirey Francis <thecursedmerchant@gmail.com>
2018-10-07 13:09:24 -0400

    Changed the authorization token to save into session storage instead

--- src/app/components/accountinfo/accountinfo.component.spec.ts
+++ src/app/components/accountinfo/accountinfo.component.spec.ts
@@ -4,29 +4,23 @@ import { AccountinfoComponent } from './accountinfo.component';
 import { AppModule } from '../../app.module';
 import { AuthService } from '../../services/auth.service';
 import { UserControllerService } from '../../services/api/user-controller.service';
-import { HttpHandler, HttpClient } from '@angular/common/http';
 
 describe('AccountinfoComponent', () => {
   let component: AccountinfoComponent;
   let fixture: ComponentFixture<AccountinfoComponent>;
 
-  // beforeEach(async(() => {
-  //   TestBed.configureTestingModule({
-  //     declarations: [ AccountinfoComponent ],
-  //     providers: [AuthService, UserControllerService],
-  //   })
-  //   .compileComponents();
-  // }));
-
-  // beforeEach(() => {
-  //   fixture = TestBed.createComponent(AccountinfoComponent);
-  //   component = fixture.componentInstance;
-  //   fixture.detectChanges();
-  // });
+  beforeEach(async(() => {
+    TestBed.configureTestingModule({
+      declarations: [ AccountinfoComponent ],
+      providers: [AuthService, UserControllerService],
+    })
+    .compileComponents();
+  }));
 
   beforeEach(() => {
-    TestBed.configureTestingModule({providers: [HttpHandler, HttpClient, UserControllerService]});
-    component = TestBed.get(UserControllerService);
+    fixture = TestBed.createComponent(AccountinfoComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
   });
 
   it('should create', () => {
