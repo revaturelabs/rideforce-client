2a49a9a99f45ee801c8f00a23b77f884de3b7aac
John Jacko <jljacko@outlook.com>
2018-10-10 10:09:38 -0400

    Added Comments to car registration and login components, also set it up for document analysis with Compo docs

--- src/app/components/car-registration/car-registration.component.ts
+++ src/app/components/car-registration/car-registration.component.ts
@@ -3,6 +3,9 @@ import { Car } from '../../models/car.model';
 import { UserControllerService } from '../../services/api/user-controller.service';
 import { User } from '../../models/user.model';
 
+/**
+ * Supports the functionality of car registration and management
+ */
 @Component({
   selector: 'app-car-registration',
   templateUrl: './car-registration.component.html',
@@ -10,24 +13,60 @@ import { User } from '../../models/user.model';
 })
 export class CarRegistrationComponent implements OnInit {
 
+  /**
+   * The User that owns the car
+   */
   userObject: User;
+
   // for drivers
+  /**
+   * The car to create and register
+   */
   carObject: Car;
+  /**
+   * The company that manufactured it
+   */
   carMake: string;
+
+  /**
+   * Model of the car
+   */
   carModel: string;
+
+  /**
+   * When the car was produced
+   */
   carYear: number;
+
+  /**
+   * Relic from the AccountInfoComponent that previously managed cars
+   */
   optInToDrive: boolean;
+
+  /**
+   * List of cars owned by the ownwer to present in a table
+   */
   cars: Car[];
 
   // booleans for car information buttons
   btnCarInfo: Number = 0;
 
+  /**
+   * Sets up the Car Registration component with dependencies
+   * @param {UserControllerService} userService - the Service that allows us to manager the user AND the cars available on the system
+   */
   constructor(private userService: UserControllerService) { }
 
+  /**
+   * Makes sure there is a car object available to operate on
+   */
   ngOnInit() {
     this.carObject = new Car();
   }
 
+  /**
+   * Clears the fields in the form
+   */
   resetCars() {
 
     if (this.cars !== undefined && this.cars != null && this.cars.length !== 0) {
@@ -37,7 +76,7 @@ export class CarRegistrationComponent implements OnInit {
     const userId = this.userObject.id;
     this.cars = [];
     this.userService.getAllCars().subscribe(c => {
-      
+
       c.forEach(element => {
         const strNum = element.owner.substring(7);
         const num = Number.parseInt(strNum);
@@ -49,7 +88,9 @@ export class CarRegistrationComponent implements OnInit {
   }
 
 
-
+  /**
+   * Ensures that the component is working with the logged on user
+   */
   resetUser() {
     this.userService.getCurrentUser().subscribe(user => {
       console.log('Car reg: Gotten User: ' + user);
@@ -61,6 +102,9 @@ export class CarRegistrationComponent implements OnInit {
 
   }
 
+  /**
+   * Sets the car with appropriate values and sends it to the server using the User Service
+   */
   addCarToUser() {
 
     this.resetUser();
@@ -79,6 +123,9 @@ export class CarRegistrationComponent implements OnInit {
 
   }
 
+  /**
+   * Refreshes the fields in the Car form
+   */
   refreshFields() {
     this.carMake = '';
     this.carModel = '';
