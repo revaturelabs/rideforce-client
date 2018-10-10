08aaea8027409333aef569ed00759a6dd95321e5
jgonzd <jonathan.gonzalezdavila@lc.cuny.edu>
2018-10-10 15:27:10 -0400

    Added test to view profile. Commented out tests in navbar. Modified tests in admin and how-to components to have them render the components.

--- src/app/components/view-profile/view-profile.component.spec.ts
+++ src/app/components/view-profile/view-profile.component.spec.ts
@@ -2,12 +2,15 @@ import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 import { HttpHandler, HttpClient } from '@angular/common/http';
 import { UserControllerService } from '../../services/api/user-controller.service';
 // import { DateFormatPipe } from '../../pipes/date-format.pipe';
+import { By } from '@angular/platform-browser';
 
 import { ViewProfileComponent } from './view-profile.component';
+import { FormsModule } from '../../../../node_modules/@angular/forms';
+import { NgbModule } from '../../../../node_modules/@ng-bootstrap/ng-bootstrap';
 
 describe('ViewProfileComponent', () => {
   let component: ViewProfileComponent;
-  // let fixture: ComponentFixture<ViewProfileComponent>;
+  let fixture: ComponentFixture<ViewProfileComponent>;
 
   // beforeEach(async(() => {
   //   TestBed.configureTestingModule({
@@ -16,20 +19,46 @@ describe('ViewProfileComponent', () => {
   //   .compileComponents();
   // }));
 
-  // beforeEach(() => {
-  //   fixture = TestBed.createComponent(ViewProfileComponent);
-  //   component = fixture.componentInstance;
-  //   fixture.detectChanges();
-  // });
-
   //Got rid of DateFormatPipe from providers to make code work.
-  beforeEach(() => {
+  beforeEach(async(() => {
     TestBed.configureTestingModule({providers: [HttpHandler, HttpClient, 
-       UserControllerService, ViewProfileComponent]});
-    component = TestBed.get(ViewProfileComponent);
+       UserControllerService, ViewProfileComponent],
+      declarations:[ViewProfileComponent],
+    imports:[FormsModule, NgbModule]}).compileComponents();
+    // component = TestBed.get(ViewProfileComponent);
+  }));
+
+  // beforeEach(async(() => {
+  //   TestBed.configureTestingModule({
+  //       //declarations: [ LoginComponent ],
+  //       imports: [
+  //         AppModule
+  //         ],
+  //       providers:[
+  //         {provide: APP_BASE_HREF, useValue : '/' }
+  //       ]
+  //   })
+  //   .compileComponents();
+  // }));
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(ViewProfileComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
   });
 
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+
+  it('should make profile editable', () => {
+    component.edit();
+    const inputUser: HTMLInputElement = fixture.nativeElement.querySelector("#firstName");
+    const e: Event = document.createEvent("Event");
+    e.initEvent("input", false, false);
+    // inputUser.value = "Bob";
+    // inputUser.dispatchEvent(e);
+    // fixture.detectChanges();
+    // console.log(fixture.debugElement.query(By.css('#firstName')).nativeElement.innerText);
+  })
 });
