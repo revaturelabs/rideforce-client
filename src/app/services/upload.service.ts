f8c96eace1fb64166464b2eb544043bbd575573f
John Jacko <jljacko@outlook.com>
2018-10-12 10:45:19 -0400

    Added Comments to upload service - Documentation coverage up to 75%

--- src/app/services/upload.service.ts
+++ src/app/services/upload.service.ts
@@ -1,16 +1,28 @@
 import { Injectable } from '@angular/core';
 import * as S3 from 'aws-sdk/clients/s3';
 
+/**
+ * Enables components to upload files (cheifly images) to the dedicated S3 Bucket on Amazon Web Services
+ */
 @Injectable({
   providedIn: 'root'
 })
 export class UploadService {
 
+  /** The Root folder name used by this system */
   FOLDER = 'rydeforce-s3/';
+  /** Holds the url where the image will be stored*/
   url: any;
-  
+
+  /**
+   * Basic set up of the Service - it uses no dependency injection
+   */
   constructor() { }
 
+  /**
+   * Retrieves an S3 bucket object so we can upload files to an actual S3 bucket on Amazon
+   * @returns {S3} - the S3 representation to utillize
+   */
   getS3Bucket(): any {
     const bucket = new S3 (
     {
@@ -20,8 +32,13 @@ export class UploadService {
     }
     );
     return bucket;
-  } 
+  }
 
+  /**
+   * Attempts to actually upload a file to the S3 Service
+   * @param file - the file to upload to the S3 service
+   * @returns {string} - the url used for the file
+   */
   uploadfile(file) {
    const params = {
       Bucket: 'rydeforce',
@@ -30,7 +47,7 @@ export class UploadService {
        // allow that and instead assign numbers on the beginning of the name.
     };
 
-    let upResult = this.getS3Bucket().upload(params, function (err, data) {
+    const upResult = this.getS3Bucket().upload(params, function (err, data) {
       if (err) {
         console.log('There was an error uploading your file: ', err);
         return false;
@@ -50,4 +67,4 @@ export class UploadService {
     console.log('BEFORE RETURNING, this.url is: ' + this.url);
     return this.url;
   }
-} 
+}
