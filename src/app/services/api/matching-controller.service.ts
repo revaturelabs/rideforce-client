bad726026279230be341116161cb8fd478a29ab1
John Jacko <jljacko@outlook.com>
2018-10-15 10:07:39 -0400

    Added comments that boosted doc coverage to 82%

--- src/app/services/api/matching-controller.service.ts
+++ src/app/services/api/matching-controller.service.ts
@@ -1,20 +1,28 @@
 import { Injectable } from '@angular/core';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
-import { User } from "../../../app/models/user.model";
+import { User } from '../../../app/models/user.model';
 import { Observable, of } from 'rxjs';
 import { environment } from '../../../environments/environment';
 import { Link } from '../../models/link.model';
 import { Filter } from '../../models/filter';
 
+/**
+ * Manages User's interactions with other users on Rideshare
+ */
 @Injectable()
 export class MatchingControllerService {
 
+  /**
+   * Sets up the Service with an HTTPClient injection
+   * @param http - provides the means of interacting with the HTTP servier
+   */
   constructor(private http: HttpClient) { }
 
   /**
    * will deserialize the link object. returns the object reference from the link
    * a link is a reference to the object without having to fetch the object from the database
-   * @param uri 
+   * @param {Link<any>} uri - the mapping to a specific user
+   * @returns {Observable<any>} - anthing that might be returned
    */
   getFromLink(uri: Link<any>): Observable<any> {
     return this.http.get<any>(environment.apiUrl + uri);
@@ -22,27 +30,35 @@ export class MatchingControllerService {
   /**
    * Returns all drivers who match the rider with the given user ID.
    * "/users/{userid}"
-   * 
-   * @param riderId
+   *
+   * @param {number} riderId - the id of the rider making the request
+   * @returns {Observable<Link<User>[]>} - the list of drivers that "match" the user
    */
   getMatchingDrivers(riderId: number): Observable<Link<User>[]> {
-   
+
     return this.http.get<Link<User>[]>(environment.apiUrl + `/matches/${riderId}`);
   }
 
+  /**
+   * Returns Users who have passed the given filter and resemble the User enough to be returned
+   * @param riderId - the rider making the request
+   * @param filter - the specific filters to add to the request
+   * @returns {Promise<User[]>} - the list of Users that pass the filter specifications
+   */
   getFilteredDrivers(riderId: number, filter: Filter): Promise<User[]> {
-    let body = {
+    const body = {
       filter,
       riderId
-    }
+    };
     return this.http.post<User[]>(environment.apiUrl + `matches/filtered`, body).toPromise();
   }
 
   /**
    * Returns all users liked by the user with the given ID.
    * Returns this as an array of strings in the format
-   * 
-   * @param riderId 
+   *
+   * @param {number} riderId - the rider making the request
+   * @returns {Observable<Link<User>[]>} - list of Users the User 'liked'
    */
   getLikedDrivers(riderId: number): Observable<Link<User>[]> {
     console.log(environment.apiUrl + `/matches/likes/${riderId}`);
@@ -51,21 +67,23 @@ export class MatchingControllerService {
 
   /**
    * Indicates that the user with ID :id likes the user with ID :liked.
-   * 
-   * @param riderId 
-   * @param driverId 
+   *
+   * @param {number} riderId - the rider making the request
+   * @param {number} driverId - the driver the rider now likes
+   * @returns {Observable<string[]>} - Does not actually return anything
    */
   likeDriver(riderId: number, driverId: number): Observable<string[]> {
-    
+
     return this.http.put<Link<User>[]>(environment.apiUrl + `/matches/likes/${riderId}/${driverId}`, '');
 
   }
 
   /**
    * Removes any indication that the user with ID :id likes the user with ID :liked
-   * 
-   * @param riderId 
-   * @param driverId 
+   *
+   * @param {number} riderId - the rider making the request
+   * @param {number} driverId - the driver the rider now dislikes
+   * @returns {Observable<string[]>} - Does not actually return anything
    */
   unlikeDriver(riderId: number, driverId: number): Observable<string[]> {
     return this.http.delete<Link<User>[]>(environment.apiUrl + `/matches/likes/${riderId}/${driverId}`);
@@ -73,7 +91,8 @@ export class MatchingControllerService {
 
   /**
    * Returns all users disliked by the user with the given ID
-   * @param riderId 
+   * @param {number} riderId - the rider making the request
+   * @returns {Observable<Link<User>[]>} - the list of drivers the user disliked
    */
   getDislikedDrivers(riderId: number): Observable<Link<User>[]> {
     return this.http.get<Link<User>[]>(environment.apiUrl + `/matches/dislikes/${riderId}`);
@@ -81,9 +100,10 @@ export class MatchingControllerService {
 
   /**
    * Indicates that the user with ID :id unliked the user with ID :liked.
-   * 
-   * @param riderId 
-   * @param driverId 
+   *
+   * @param {number} riderId - the rider making the request
+   * @param {number} driverId - the driver to affect
+   * @returns {Observable<string[]>} - Does not actually return anything
    */
   dislikeDriver(riderId: number, driverId: number): Observable<string[]> {
     return this.http.put<Link<User>[]>(environment.apiUrl + `/matches/dislikes/${riderId}/${driverId}`, '');
@@ -91,9 +111,10 @@ export class MatchingControllerService {
 
   /**
    *  Removes any indication that the user with ID :id disliked the user with ID :liked
-   * 
-   * @param riderId 
-   * @param driverId 
+   *
+   * @param {number} riderId - the rider making the request
+   * @param {number} driverId - the driver to affect
+   * @returns {Observable<string[]>} - Does not actually return anything
    */
   unDislikeDriver(riderId: number, driverId: number): Observable<string[]> {
     return this.http.delete<Link<User>[]>(environment.apiUrl + `/matches/dislikes/${riderId}/${driverId}`);
--- src/app/utils/token.storage.ts
+++ src/app/utils/token.storage.ts
@@ -1,17 +1,30 @@
 import { Injectable } from '@angular/core';
 
+/** Used to access the Authentication token in our session storage */
 const TOKEN_KEY = 'authToken';
 
+/** Provides basic methods for accessing the session token */
 @Injectable({
   providedIn: 'root'
 })
 export class TokenStorage {
+  /**
+   * @ignore
+   */
   constructor() { }
 
+  /**
+   * Saves the provided authentication token to the session
+   * @param {string} token - the authentication token to save
+   */
   public saveToken(token: string) {
     sessionStorage.setItem(TOKEN_KEY, token);
   }
 
+  /**
+   * Retrieves the current authentication token being used
+   * @returns {string} - the authentication token of the current session
+   */
   public getToken(): string {
     return sessionStorage.getItem(TOKEN_KEY);
   }
