06f751e2b5eb6077ce20e27d154e8359d85f12dc
jgonzd <jonathan.gonzalezdavila@lc.cuny.edu>
2018-10-17 04:02:30 -0400

    saving

--- src/app/components/usercard/usercard.component.spec.ts
+++ src/app/components/usercard/usercard.component.spec.ts
@@ -2,12 +2,16 @@ import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 import { HttpHandler, HttpClient } from '@angular/common/http';
 import { UserControllerService } from '../../services/api/user-controller.service';
 import { MatchingControllerService } from '../../services/api/matching-controller.service';
+import { ElementRef } from '@angular/core';
+
 
 import { UsercardComponent } from './usercard.component';
+import { AppModule } from '../../app.module';
+import { APP_BASE_HREF } from '../../../../node_modules/@angular/common';
 
 describe('UsercardComponent', () => {
   let component: UsercardComponent;
-  // let fixture: ComponentFixture<UsercardComponent>;
+  let fixture: ComponentFixture<UsercardComponent>;
 
   // beforeEach(async(() => {
   //   TestBed.configureTestingModule({
@@ -17,17 +21,48 @@ describe('UsercardComponent', () => {
   // }));
 
   // beforeEach(() => {
-  //   fixture = TestBed.createComponent(UsercardComponent);
-  //   component = fixture.componentInstance;
-  //   fixture.detectChanges();
+  //   TestBed.configureTestingModule({providers: [HttpHandler, HttpClient, MatchingControllerService,
+  //      UserControllerService, UsercardComponent]});
+  //   component = TestBed.get(UsercardComponent);
   // });
-  beforeEach(() => {
-    TestBed.configureTestingModule({providers: [HttpHandler, HttpClient, MatchingControllerService,
-       UserControllerService, UsercardComponent]});
-    component = TestBed.get(UsercardComponent);
+
+  beforeEach(async(() => {
+    TestBed.configureTestingModule({
+        imports: [
+          AppModule
+          ],
+        providers: [
+          {provide: APP_BASE_HREF, useValue : '/' ,
+          UsercardComponent}
+        ]
+    })
+    .compileComponents();
+  }));
+     beforeEach(() => {
+    fixture = TestBed.createComponent(UsercardComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
   });
 
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+
+  it('hide image tests', () => {
+    const spyObj = jasmine.createSpyObj('component', ['nativeElement']);
+    // let elRef: ElementRef;
+    // component.swipeCardMain = elRef;
+    component.hideImage(true);
+    //expect(component.swipeCardMain).toBeTruthy();
+  });
+   it('unhide image tests', () => {
+    component.hideImage(false);
+   });
+    it('swipe action right',() => {
+     component.swipe(component.SWIPE_ACTION.RIGHT,null);
+   })
+    it('swipe action left',() => {
+    component.swipe(component.SWIPE_ACTION.LEFT,null);
+  })
+
 });
