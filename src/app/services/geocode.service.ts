a34c278398917d6120dcfd40aa6fe37bb3fe3de3
Pantherhawk <alexrosenthalster@gmail.com>
2018-12-18 09:47:46 -0500

    get request to location service in map geocode serviec

--- src/app/services/geocode.service.ts
+++ src/app/services/geocode.service.ts
@@ -1,5 +1,6 @@
 import { Injectable } from '@angular/core';
 import { Observable, Observer } from 'rxjs';
+import { HttpClient } from '@angular/common/http';
 
 /**
  * Provides Specific geolocation services from Google maps
@@ -9,6 +10,9 @@ import { Observable, Observer } from 'rxjs';
 })
 export class GeocodeService {
 
+  dev: boolean = true;
+  endpoint: string = this.dev ? 'http://localhost:3333/location/?address=' : 'http://ec2-35-174-153-234.compute-1.amazonaws.com:3333/location/?address=';
+
   /**
    * Enables communication with Google Map services
    */
@@ -17,8 +21,8 @@ export class GeocodeService {
   /**
    * Sets up the Service with a Google Maps object
    */
-  constructor() {
-    this.geocoder = new google.maps.Geocoder();
+  constructor(private http: HttpClient) {
+
   }
 
   /**
@@ -48,24 +52,6 @@ export class GeocodeService {
    * @param address - the address of a location to obtain information about
    * @returns {Observable<google.maps.GeocoderResult[]>} - information about your given location
    */
-  geocode(address: string): Observable<google.maps.GeocoderResult[]> {
-    return Observable.create((observer: Observer<google.maps.GeocoderResult[]>) => {
-      // Invokes geocode method of Google Maps API geocoding.
-      this.geocoder.geocode({ address: address }, (
-        (results: google.maps.GeocoderResult[], status: google.maps.GeocoderStatus) => {
-          if (status === google.maps.GeocoderStatus.OK) {
-            observer.next(results);
-            observer.complete();
-          } else {
-            console.log(
-              'Geocoding service: geocode was not successful for the following reason: '
-              + status
-            );
-            observer.error(status);
-          }
-        })
-      );
-    });
-    return null;
-  }
+  geocode(address: string): Observable<object> {
+    return this.http.get(this.endpoint);
 }
