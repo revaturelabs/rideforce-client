857dffbe1b21105e8e3cb3a6d32b52e72407bd1c
Mateusz Wiater <mateuszwiater@gmail.com>
2019-02-04 16:03:52 -0500

    Forgot to add https to prod.

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -2,9 +2,9 @@
 export const environment = {
   /** whater instance is in production mode (true in this case) */
   production: true,
-  mapUrl: 'http://rideforce-alb-1943696763.us-east-1.elb.amazonaws.com/maps',
-  matchUrl: 'http://rideforce-alb-1943696763.us-east-1.elb.amazonaws.com/matching',
-  userUrl: 'http://rideforce-alb-1943696763.us-east-1.elb.amazonaws.com/user',
+  mapUrl: 'https://rideforce-alb-1943696763.us-east-1.elb.amazonaws.com/maps',
+  matchUrl: 'https://rideforce-alb-1943696763.us-east-1.elb.amazonaws.com/matching',
+  userUrl: 'https://rideforce-alb-1943696763.us-east-1.elb.amazonaws.com/user',
   cognitoData : {
     UserPoolId: 'us-east-1_lyIcVP12k',
     ClientId: 'pjkpr0p3d2d8qlklehu9d3hic'
