fe6d581c0237d3dd87e0618e84bb92052b86f0ae
dehkrueger <drrueger@wpi.edu>
2019-02-05 16:15:25 -0500

    Fixed call to matchAPI that should have been sent to userAPI

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -9,11 +9,11 @@ export const environment = {
   production: false,
 /** URL for the service we are relying on */
   mapUrl: 'https://apirideforce.revaturelabs.com/maps',
-  matchUrl: 'https://apirideforce.revaturelabs.com/matching',
+  //matchUrl: 'https://apirideforce.revaturelabs.com/matching',
   userUrl: 'https://apirideforce.revaturelabs.com/user',
-  // mapUrl: 'http://localhost:3333',
-  // matchUrl: 'http://localhost:4444',
-  // userUrl: 'http://localhost:5555',
+  //mapUrl: 'http://localhost:3333',
+  matchUrl: 'http://localhost:4444',
+  //userUrl: 'http://localhost:5555',
   cognitoData : {
     UserPoolId: 'us-east-1_lyIcVP12k',
     ClientId: 'pjkpr0p3d2d8qlklehu9d3hic'
