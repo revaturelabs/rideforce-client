8fcd8653a25925e6e2fa87bd6d5787cfb875b555
Mateusz Wiater <mateuszwiater@gmail.com>
2019-02-08 16:14:44 -0500

    Fixed issue where profile pictures did not show up in the browse results.

--- src/app/components/usermatchweb/usermatchweb.component.ts
+++ src/app/components/usermatchweb/usermatchweb.component.ts
@@ -2,7 +2,7 @@ import { animate, state, style, transition, trigger } from '@angular/animations'
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import { NgxSpinnerService } from 'ngx-spinner';
-import { Login } from "../../models/login.model";
+import { Login } from '../../models/login.model';
 import { Link } from '../../models/link.model';
 import { User } from '../../models/user.model';
 import { MatchingControllerService } from '../../services/api/matching-controller.service';
@@ -11,7 +11,6 @@ import { AuthService } from '../../services/auth.service';
 import { GeocodeService } from '../../services/geocode.service';
 import { DownloadService } from '../../services/download.service';
 import { DomSanitizer } from '@angular/platform-browser';
-import { userInfo } from 'os';
 
 
 /** Represents the User selection item in the html page */
@@ -20,8 +19,10 @@ interface DriverCard {
   user: User;
   /** The status of the given user */
   choose: string;
-  /** Link to profile picture of the user */
+  /** Toggle for the card */
   face: String;
+  /** Card image */
+  image: any;
   /**The calculated distance for sorting with geolocation */
   distance: number;
 }
@@ -55,7 +56,7 @@ export class UsermatchwebComponent implements OnInit {
   /** to store user location */
   myLocation: object = null;
 
-  principal : Login;
+  principal: Login;
   /**
      * Sets up Component with the Matching and User services injected
      * @param {MatchingControllerService} matchService - Enables the matching service
@@ -67,9 +68,9 @@ export class UsermatchwebComponent implements OnInit {
     private route: Router,
     private spinner: NgxSpinnerService,
     private geocodeService: GeocodeService,
-    private auth : AuthService,
-    private DLService: DownloadService,
-    public SanizeService: DomSanitizer
+    private auth: AuthService,
+    private ds: DownloadService,
+    public ss: DomSanitizer
   ) { }
 
   /** Holds the current user of the system */
@@ -83,21 +84,21 @@ export class UsermatchwebComponent implements OnInit {
   filterDistance: boolean = false;
   /**If page is loading */
   loading: boolean;
-  
+
   private imageFile: any;
 
   /**
    * Sets up the component by populating the list of possibel matches for the current user
    */
   ngOnInit() {
-    this.auth.principal.subscribe(user =>{
+    this.auth.principal.subscribe(user => {
       this.principal = user;
     if (this.principal.id < 1) {
       this.route.navigate(['/landing']);
     }
     this.spinner.show();
     this.loading = true;
-    console.log("Loading: " + this.loading);
+    console.log('Loading: ' + this.loading);
     this.userService.getCurrentUser().subscribe(
       data => {
         this.currentUser = data;
@@ -105,32 +106,28 @@ export class UsermatchwebComponent implements OnInit {
         let userLinks: Link<User>[] = null;
         this.matchService.getMatchingDrivers(+(this.principal.id)).subscribe(
           data2 => {
-            // console.log("data2 is " + data2);
             userLinks = data2;
             console.log(userLinks);
             for (let i = 0; i < userLinks.length; i++) {
 
               this.matchService.getFromLink(userLinks[i]).subscribe(
                 data3 => {
-                  if (!data3.photoUrl || data3.photoUrl === 'null') {
-                    data3.photoUrl = 'http://semantic-ui.com/images/avatar/large/chris.jpg';
-                  }else{
-                    this.imageFile = this.DLService.downloadFile(data3.id.toString);
-                  }
                   const card: DriverCard = {
                     user: data3,
                     choose: 'none',
                     face: 'front',
-                    distance: null
+                    distance: null,
+                    image: 'http://semantic-ui.com/images/avatar/large/chris.jpg'
                   };
+                  this.populateProfileImage(card);
                   // Sets the current swipe card to the first element of the array if the array has something in it.
                   this.users.push(card);
                   // assign drivers to the list to render and shuffle
                   this.sortedUsers = this.users;
                   // sets loading to false
-                  this.users.forEach(user => this.appendLocation(user));
+                  this.users.forEach(u => this.appendLocation(u));
                   this.loading = false;
-                  //hides the spinner
+                  // hides the spinner
                   this.spinner.hide();
                 },
                 e => {
@@ -229,31 +226,30 @@ export class UsermatchwebComponent implements OnInit {
     this.sortedUsers = this.shuffle(this.users);
 
     const filterMap = {
-      "starttime": () => {
-        console.log('sorting by start time')
-        this.sortedUsers = this.users.sort((a, b) => a.user.startTime - b.user.startTime)
+      'starttime': () => {
+        console.log('sorting by start time');
+        this.sortedUsers = this.users.sort((a, b) => a.user.startTime - b.user.startTime);
       },
-      "batchend": () => {
+      'batchend': () => {
         console.log('sorting by batch end');
-        this.sortedUsers = this.users.sort((a, b) => new Date(b.user.batchEnd).getTime() - new Date(a.user.batchEnd).getTime())
+        this.sortedUsers = this.users.sort((a, b) => new Date(b.user.batchEnd).getTime() - new Date(a.user.batchEnd).getTime());
       },
-      //TODO: Schwartzian transform to optimize...
-      "distance": () => {
+      'distance': () => {
         console.log('sorting by distance');
         this.sortedUsers = this.users.sort((a, b) => b.distance - a.distance).reverse();
       }
+    };
 
-    }
     if (this.filterStartTime || this.filterDistance || this.filterBatchEnd) {
-        console.log('going to filter')
-        options.forEach(tuple => {
-          if (tuple[0] === true) {
-            console.log('sorting by ' + tuple[1])
-            const value = tuple[1]
-            filterMap[value.toString()]();
-          }
-        })
-        console.log("after sorting: ", this.sortedUsers)
+      console.log('going to filter');
+      options.forEach(tuple => {
+        if (tuple[0] === true) {
+          console.log('sorting by ' + tuple[1]);
+          const value = tuple[1];
+          filterMap[value.toString()]();
+        }
+      });
+      console.log('after sorting: ', this.sortedUsers);
       } else {
         this.sortedUsers = this.users;
       }
@@ -262,42 +258,40 @@ export class UsermatchwebComponent implements OnInit {
     calculateDistance(x1: number, x2: number, y1: number, y2: number): number {
       // const distance: number = Math.sqrt(Math.pow((x2 - x1), 2)/Math.pow((y2 - y1), 2));
 
-      const radlat1 = Math.PI * y1/180;
-      const radlat2 = Math.PI * y2/180;
-      const radlong1 = Math.PI * x1/180;
-      const radlong2 = Math.PI * x2/180;
+      const radlat1 = Math.PI * y1 / 180;
+      const radlat2 = Math.PI * y2 / 180;
+      const radlong1 = Math.PI * x1 / 180;
+      const radlong2 = Math.PI * x2 / 180;
       const theta = x1 - x2;
-      const radtheta = Math.PI * theta/180;
-      var distance = Math.sin(radlat1) * Math.sin(radlat2) + Math.cos(radlat1) * Math.cos(radlat2) * Math.cos(radtheta);
+      const radtheta = Math.PI * theta / 180;
+      let distance = Math.sin(radlat1) * Math.sin(radlat2) + Math.cos(radlat1) * Math.cos(radlat2) * Math.cos(radtheta);
       distance = Math.acos(distance);
-      distance = distance * 180/Math.PI;
+      distance = distance * 180 / Math.PI;
       distance = distance * 60 * 1.1515;
       return distance;
-
     }
 
     // get the address and append it
     async appendLocation(user) {
       const myLocation = await this.geocodeService.geocode(this.principal.address).toPromise();
       const location = await this.geocodeService.geocode(user.user.address).toPromise();
-      user["distance"] = this.calculateDistance(
-        myLocation["lng"],
-        location["lng"],
-        myLocation["lat"],
-        location["lat"]
+      user['distance'] = this.calculateDistance(
+        myLocation['lng'],
+        location['lng'],
+        myLocation['lat'],
+        location['lat']
         );
         console.log('usre with appended distance: ' + user);
         return user;
     }
 
     async getLngLat(address: string): Promise<number> {
-      let uLongitude: number, uLatitude: number;
       const otherLocation = await this.geocodeService.geocode(address).toPromise();
       const myLocation = await this.geocodeService.geocode(this.principal.address).toPromise();
-      const x1 = myLocation["lng"];
-      const x2 = otherLocation["lng"];
-      const y1 = myLocation["lat"];
-      const y2 = otherLocation["lat"];
+      const x1 = myLocation['lng'];
+      const x2 = otherLocation['lng'];
+      const y1 = myLocation['lat'];
+      const y2 = otherLocation['lat'];
       return this.calculateDistance(x1, x2, y1, y2);
     }
 
@@ -310,36 +304,17 @@ export class UsermatchwebComponent implements OnInit {
     }
 
     shuffle(list: DriverCard[]): Array<DriverCard> {
-      var m = list.length, t: DriverCard, i: number;
+      let m = list.length, t: DriverCard, i: number;
       while (m) {
         i = Math.floor(Math.random() * m--);
-
         t = list[m];
         list[m] = list[i];
         list[i] = t;
       }
       return list;
-    }
-
-    downloadFile(target: User){
-      this.DLService.downloadFile(target.id.toString()).subscribe(resp => {
-        this.createImageFromBlob(resp);
-      }, error => {
-        console.log(error);
-      });
-  
-    console.log('Download service called');
-    //console.log(this.imageFile);
   }
-
-  createImageFromBlob(image: Blob) {
-    const reader = new FileReader();
-    reader.addEventListener('load', () => {
-       this.imageFile = reader.result;
-    }, false);
   
-    if (image) {
-       reader.readAsDataURL(image);
-    }
+  populateProfileImage(dc: DriverCard) {
+    this.ds.downloadFile(dc.user.id.toString()).subscribe(b => dc.image = this.ss.bypassSecurityTrustUrl(window.URL.createObjectURL(b)));
   }
 }
