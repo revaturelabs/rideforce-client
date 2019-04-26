88319fc61b96ad5693c4c621e0c0d55e729efc4f
unknown <iapchi86@gmail.com>
2019-04-26 16:06:14 -0400

    added functionality

--- src/app/models/registration-token.model.ts
+++ src/app/models/registration-token.model.ts
@@ -5,6 +5,19 @@ export class RegistrationToken {
   batchEndDate: string;
 
   constructor() {
-    this.batchEndDate = new Date(Date.now()).toISOString().split('T')[0];
+	  var today = new Date();
+	  var day = today.getDay() || 3;
+	  if (day !== 1) {
+		  today.setHours(-24 * (day - 5));
+		  /*var friday = today.setHours(-24 * (day - 5))*/
+		  /*today.setDate(today.setHours(-24 * (day - 5)) + 70);*/
+		  //Friday currently
+		  var tenweeks = new Date(new Date(today).getTime() + (70 * 24 * 60 * 60 * 1000));
+		  this.batchEndDate = new Date(tenweeks).toISOString().split('T')[0];
+	  }
+	  else {
+		  var tenweeks = new Date(new Date().getTime() + (70 * 24 * 60 * 60 * 1000));
+		  this.batchEndDate = new Date(tenweeks).toISOString().split('T')[0];
+	  }
   }
 }
