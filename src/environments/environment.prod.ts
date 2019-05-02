02a0ef031ad0a431e17d60e1250cc534d71f1a46
tnawshin <tawbin.nawshin@baruchmail.cuny.edu>
2019-05-02 12:17:27 -0400

    vehicle form

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -2,12 +2,21 @@
 export const environment = {
   /** whater instance is in production mode (true in this case) */
   production: true,
+<<<<<<< Updated upstream
   mapUrl: 'http://rideforce-dev-2113170582.us-east-1.elb.amazonaws.com/maps',
   matchUrl: 'http://rideforce-dev-2113170582.us-east-1.elb.amazonaws.com/matching',
   userUrl: 'http://rideforce-dev-2113170582.us-east-1.elb.amazonaws.com/user',
   // mapUrl: 'http://localhost:3333',
   // matchUrl: 'http://localhost:4444',
   // userUrl: 'http://localhost:5555',
+=======
+  //mapUrl: 'http://rideforce-alb-1943696763.us-east-1.elb.amazonaws.com/maps',
+  //matchUrl: 'http://rideforce-alb-1943696763.us-east-1.elb.amazonaws.com/matching',
+  //userUrl: 'http://rideforce-alb-1943696763.us-east-1.elb.amazonaws.com/user',
+  mapUrl: 'http://localhost:3333',
+  matchUrl: 'http://localhost:4444',
+  userUrl: 'http://localhost:5555',
+>>>>>>> Stashed changes
   cognitoData : {
     UserPoolId: 'us-east-1_lyIcVP12k',
     ClientId: 'pjkpr0p3d2d8qlklehu9d3hic'
