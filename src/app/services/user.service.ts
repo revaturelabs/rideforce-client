23de3c77cc27cc2bff42889c9069d1b57fafa2bb
Brita Ramsay <britaramsay@gmail.com>
2019-10-25 11:25:35 -0400

    Updateed login

--- src/app/services/user.service.ts
+++ src/app/services/user.service.ts
@@ -15,11 +15,12 @@ export class UserService {
   /**
    * use headers in http calls to circumvent 
    */
-  private headers = new HttpHeaders({
+  private head = new HttpHeaders({
     'Content-Type': 'application/json',
-    'x-rapidapi-host': 'omgvamp-hearthstone-v1.p.rapidapi.com',
-    'x-rapidapi-key': '87de82526cmshe4c5e6d4b8edcedp126831jsn193b85d91e9b'
+    'Access-Control-Allow-Origin': '*'
   });
+
+  private options = { headers: this.head }
   /**
    * Sets up the User Service via the Injection of the HttpClient
    * @param {HttpClient} http - Allows service to communicate with the server via HTTP requests
@@ -35,13 +36,12 @@ export class UserService {
   // private users: User[] = [];
 
 
-  login(user: User) {
-    localStorage.setItem('currentUser', JSON.stringify(user));
-    return this.http.post<User>('enviornment.userUrl' + '/users', JSON.stringify(user));
+  register(user: User) {
+    return this.http.post<User>(this.host + '/users', user, this.options);
   }
 
-  register(user: User) {
-    return this.http.post<User>(this.host + '/users', user, {headers: this.headers});
+  login(user : User) : Observable<User> {
+    return this.http.post<User>(this.host + '/users/login', user, this.options);
   }
 
   logout() {
