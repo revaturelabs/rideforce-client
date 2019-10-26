4555dba471adb07001ba2af13ef032d45380671a
Michael Rollberg <mrollberg@ymail.com>
2019-10-26 11:01:18 -0400

    Basic filter for distance on browse drivers page

--- src/app/components/view-users/view-users.component.ts
+++ src/app/components/view-users/view-users.component.ts
@@ -1,7 +1,7 @@
 import { Router } from '@angular/router';
 import { Role } from '../../models/role';
 import { User } from '../../models/user';
-import { Component, OnInit, Injectable } from '@angular/core';
+import { Component, OnInit, Injectable, ChangeDetectorRef } from '@angular/core';
 import { UserService } from '../../services/user.service';
 import { Observable } from 'rxjs';
 import { MapsService } from '../../services/maps.service';
@@ -14,13 +14,14 @@ import { MapsService } from '../../services/maps.service';
 @Injectable()
 export class ViewUsersComponent implements OnInit {
   /** Holds the list of all users in the system */
-  users: User[] = [];
+  users: any[] = [];
 
 
   constructor(
     private router: Router,
     private userService: UserService,
-    private mapsService: MapsService
+    private mapsService: MapsService,
+    private cdr: ChangeDetectorRef
   ) { }
 
   // add a method allDrivers in userService(), show the ones that are within 20 miles.
@@ -29,6 +30,7 @@ export class ViewUsersComponent implements OnInit {
   }
 
   allDrivers: Observable<User[]> = this.userService.allDrivers();
+  vicinityDrivers: any[] = [];
 
 
   showAllDrivers() {
@@ -59,13 +61,15 @@ export class ViewUsersComponent implements OnInit {
           isActive: true
         };
         this.users.forEach(user => {
-          console.log("Hello");
           var origin = new google.maps.LatLng(currentUser.location.latitude, currentUser.location.longitude);
           var destination = new google.maps.LatLng(user.location.latitude, user.location.longitude);
-          new google.maps.DistanceMatrixService().getDistanceMatrix({ 'origins': [origin], 'destinations': [destination], travelMode: google.maps.TravelMode.DRIVING, unitSystem: google.maps.UnitSystem.IMPERIAL  }, (results: any) => {
-            console.log("hello2");
+          new google.maps.DistanceMatrixService().getDistanceMatrix({ 'origins': [origin], 'destinations': [destination], travelMode: google.maps.TravelMode.DRIVING, unitSystem: google.maps.UnitSystem.IMPERIAL }, (results: any) => {
             console.log(results);
             user.distance = results.rows[0].elements[0].distance.text;
+            this.vicinityDrivers.push(user);
+            this.vicinityDrivers.sort(this.sortDriversByDistance);
+            console.log(this.vicinityDrivers);
+            this.cdr.detectChanges();
 
 
           });
@@ -88,6 +92,57 @@ export class ViewUsersComponent implements OnInit {
       //optional third function for what to do when
       //the call is complete
     );
+  }
+
+  filterDrivers(vicinity: any) {
+    console.log(vicinity);
+    const newVicinityDrivers = this.users.filter(user => {
+      if (user.distance !== undefined) {
+        if (vicinity === "") {
+          return true;
+        }
+        else {
+          const distanceParts = user.distance.split(" ", 2);
+          if (distanceParts[1] === "mi") {
+            if (vicinity >= parseFloat(distanceParts[0])) {
+              return true;
+            }
+          }
+          else if (distanceParts[1] === "ft") {
+            const vicinityDistanceFt = vicinity * 5280;
+            if (vicinityDistanceFt >= parseFloat(distanceParts[0])) {
+              return true;
+            }
+          }
+        }
+      }
+    });
+
+    newVicinityDrivers.sort(this.sortDriversByDistance);
+
+    this.vicinityDrivers = newVicinityDrivers;
+    this.cdr.detectChanges();
+  }
+
+  sortDriversByDistance(a: any, b: any) {
+    const aDistanceParts = a.distance.split(" ", 2);
+    const bDistanceParts = b.distance.split(" ", 2);
+
+    if (aDistanceParts[1] === "mi") {
+      aDistanceParts[0] *= 5280;
+    }
+    if (bDistanceParts[1] === "mi") {
+      bDistanceParts[0] *= 5280;
+    }
+    if (aDistanceParts[0] < bDistanceParts[0]) {
+      return -1;
+    }
+    else if (aDistanceParts[0] > bDistanceParts[0]) {
+      return 1;
+    }
+    else {
+      return 0;
+    }
   }
 
 }
